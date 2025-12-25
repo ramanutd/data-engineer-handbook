8139df1626be9483d350f2593559259d5feca58c
Ramandeep Singh <ramanvirdiz@gmail.com>
2025-12-25 20:28:52 +0530

    feat: create user_devices_cumulated table for tracking device activity

---
+++ intermediate-bootcamp/materials/2-fact-data-modeling/homework/task-2.sql
@@ -0,0 +1,10 @@
+-- DDL for user_devices_cumulated table
+
+CREATE TABLE user_devices_cumulated
+(
+    user_id         NUMERIC,
+    device_activity DATE[],
+    browser_type    TEXT,
+    date            DATE,
+    PRIMARY KEY (user_id, browser_type, date)
+);
